a7b7a24b686f565cea979de2a9dd166cf5faf822
Lloyd P Babu <lloyd@thinkrisk.ai>
2022-08-22 00:05:39 +0530

    Added search bar and query timer delay

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import SearchInput from "../SearchInput/SearchInput";
 
-const Header = () => {
+type Props = {
+  setQuery?: React.Dispatch<React.SetStateAction<string>>
+};
+
+const Header = ({setQuery}: Props) => {
   return(
     <div className="sticky flex top-0 z-40 w-full h-24 bg-zinc-900">
     <div className="flex justify-between w-full h-full max-w-7xl px-4 m-auto">
@@ -15,6 +20,11 @@ const Header = () => {
           </div>
         </div>
       </Link>
+      {setQuery?
+      <div className="relative flex items-center">
+        <SearchInput setQuery={setQuery}/>
+      </div>
+      :null}
     </div>
   </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header/Header'
+import Hero from '../components/Hero/Hero'
+import Card from '../components/Card/Card'
+import Grid from '../components/Grid/Grid'
+import Spinner from '../components/Spinner/Spinner'
+import { useState } from 'react'
+import { useFetchMovies } from '../api/fetchHooks'
+
 
 const Home: NextPage = () => {
+  const [query, setQuery] = useState('');
+
+  const {data, fetchNextPage, isLoading, isFetching, error} = useFetchMovies(query);
+
+  console.log("data:",data);
+
   return (
    <main>
-    <Header />
-    RMDB
+    <Header setQuery={setQuery}/>
+    <Hero />
+    <Grid />
+    <Card />
+    <Spinner />
    </main>
   )
 }
